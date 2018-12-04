ea5c1ddfd623590811da26f76e0ccd41a7c1dd3f
play <790582607@qq.com>
2018-12-04 09:55:52 +0800

    添加 gulp-open gulp-connent

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const connect = require('gulp-connect')
+const open = require('gulp-open')
 
 gulp.task('sass', () => {
     return gulp
         .src('./dev/*.scss')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(gulp.dest('./dev'))
+        .pipe(connect.reload())
 })
 
-gulp.task('default', function () {
-    gulp.watch(['./dev/**/*.scss', './src/**/*.scss'], {}, ['sass'])
+gulp.task('watch', () => {
+    gulp.watch(['./dev/**/*', './src/**/*'], {}, ['sass'])
 })
+
+gulp.task('server', () => {
+    connect.server({
+        port: 8080,
+        livereload: true,
+        root: 'dev',
+    })
+})
+
+gulp.task('open', () => {
+    return gulp
+        .src('')
+        .pipe(open({
+            uri: 'http://localhost:8080',
+        }))
+})
+
+gulp.task('default', ['open', 'server', 'watch'])
